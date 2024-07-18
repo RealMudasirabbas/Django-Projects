1e3fd908524f355ebe8ddeced23ef176111ebd15
Mudasir Abbas <mudasirabbas5638201@gmail.com>
2024-07-18 20:03:42 +0500

    django project completed

---
+++ tweet_app/tweet/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Tweet,User
+from django.contrib.auth.forms import UserCreationForm
+
+class TweetForm(forms.ModelForm):
+    class Meta:
+        model = Tweet
+        fields = ['text','photo']
+
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ('username','email','password1','password2')
